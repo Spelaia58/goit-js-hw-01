02856a00f76566fce85ebb531f86173206246e5e
spelaia58 <spelaia58@gmail.com>
2020-01-17 21:05:33 +0200

    w1

---
+++ task-1.js
@@ -0,0 +1,13 @@
+let price;
+const name = 'Генератор защитного поля';
+price = 1000;
+
+let sameMessage = `Выбран ${name} , цена за штуку ${price} кредитов.`;
+
+console.log(sameMessage);
+
+price = 2000;
+
+sameMessage = `Выбран ${name} , цена за штуку ${price} кредитов.`;
+
+console.log(sameMessage);
